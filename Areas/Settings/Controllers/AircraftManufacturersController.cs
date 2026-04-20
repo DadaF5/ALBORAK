c362fd94b1d767087d3c8b9be1720d1637e9d3f4
DadaF5 <ziyadfadwa@gmail.com>
2026-04-20 23:20:14 +0200

    Lookup tables phase 1

---
+++ Areas/Settings/Controllers/AircraftManufacturersController.cs
@@ -0,0 +1,132 @@
+using FRAProject.Areas.Settings.Models;
+using FRAProject.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FRAProject.Areas.Settings.Controllers
+{
+    [Area("Settings")]
+    public class AircraftManufacturersController : Controller
+    {
+        private readonly FRAContext _context;
+
+        public AircraftManufacturersController(FRAContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Settings/AircraftManufacturers
+        public async Task<IActionResult> Index()
+        {
+            var list = await _context.AircraftManufacturers
+                .OrderBy(x => x.SortOrder)
+                .ThenBy(x => x.Name)
+                .ToListAsync();
+
+            return View(list);
+        }
+
+        // GET: Settings/AircraftManufacturers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var entity = await _context.AircraftManufacturers
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (entity == null) return NotFound();
+
+            return View(entity);
+        }
+
+        // GET: Settings/AircraftManufacturers/Create
+        public IActionResult Create()
+        {
+            return View(new AircraftManufacturer());
+        }
+
+        // POST: Settings/AircraftManufacturers/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(AircraftManufacturer entity)
+        {
+            if (await _context.AircraftManufacturers.AnyAsync(x => x.Code == entity.Code))
+            {
+                ModelState.AddModelError(nameof(entity.Code), "Code already exists.");
+            }
+
+            if (!ModelState.IsValid) return View(entity);
+
+            _context.AircraftManufacturers.Add(entity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Settings/AircraftManufacturers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var entity = await _context.AircraftManufacturers.FindAsync(id);
+            if (entity == null) return NotFound();
+
+            return View(entity);
+        }
+
+        // POST: Settings/AircraftManufacturers/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, AircraftManufacturer entity)
+        {
+            if (id != entity.Id) return NotFound();
+
+            if (await _context.AircraftManufacturers.AnyAsync(x => x.Id != entity.Id && x.Code == entity.Code))
+            {
+                ModelState.AddModelError(nameof(entity.Code), "Code already exists.");
+            }
+
+            if (!ModelState.IsValid) return View(entity);
+
+            try
+            {
+                _context.Update(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await _context.AircraftManufacturers.AnyAsync(e => e.Id == entity.Id))
+                    return NotFound();
+                throw;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Settings/AircraftManufacturers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var entity = await _context.AircraftManufacturers
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (entity == null) return NotFound();
+
+            return View(entity);
+        }
+
+        // POST: Settings/AircraftManufacturers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var entity = await _context.AircraftManufacturers.FindAsync(id);
+            if (entity != null)
+            {
+                _context.AircraftManufacturers.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
